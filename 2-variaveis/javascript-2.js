26284f00ef309c1d6006532f0e4f4b4bf5670ec1
ViniciusLeiteCosta <viniciusleitecosta@gmail.com>
2023-11-21 14:44:31 -0300

    Adding lesson n2

---
+++ 2-variaveis/javascript-2.js
@@ -0,0 +1,9 @@
+//variáveis
+let easteregg = 'Vinicius esteve aqui!';
+console.log(easteregg);
+
+let primeiroNome = "Vinicius "
+let segundoNome = "Leite"
+
+//concatenação
+console.log(primeiroNome + segundoNome);
