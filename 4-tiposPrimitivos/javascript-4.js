0057fb471b7facd2e136a9ccb6ea755b14bc9f3a
ViniciusLeiteCosta <viniciusleitecosta@gmail.com>
2023-11-21 15:19:14 -0300

    Adding lesson n4

---
+++ 4-tiposPrimitivos/javascript-4.js
@@ -0,0 +1,16 @@
+/*
+Tipos primitivos JS:
+
+String 
+Number
+Boolean
+undefined
+null
+
+*/
+
+let nome = 'CJ'
+let idade = '25'
+let aprovar = true;
+let primeiroNome = undefined;
+let corSelecionada = null;
