5c3d334a1547c66cc28b4ba0c6602698f312c393
Leite | Vinicius Leite Costa <92338016+ViniciusLeiteCosta@users.noreply.github.com>
2023-11-22 14:44:04 -0300

    Adding lesson n8

--- 8-funcoes/javascript-8.js
+++ 8-funcoes/javascript-8.js
@@ -5,7 +5,7 @@ Imagine as funções como coisas que você pode utilizar no seu código,
 chamando a mesma depois.
 
 */
-
+let pessoaAcena;
 let acenarParaEla = ' Você diz olá e com um sorriso no rosto acena! ';
 let naoAcenarParaEla = ' * Você fica quieto(a)... * ';
 
