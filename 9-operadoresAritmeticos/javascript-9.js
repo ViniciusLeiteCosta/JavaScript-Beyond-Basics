2f1a2ae79c96687a33d5742ecef579d779db0065
ViniciusLeiteCosta <viniciusleitecosta@gmail.com>
2023-11-22 16:22:25 -0300

    Adding lesson n9

---
+++ 9-operadoresAritmeticos/javascript-9.js
@@ -0,0 +1,19 @@
+/*
+
+Operadores aritméticos em JavaScript
+
+soma console.log(x+y);
+subtração console.log(x-y);
+multiplicação console.log(x*y);
+divisão console.log(x/y);
+resto da divisão console.log(x%y);
+potencialização console.log(x**y);
+
+*/
+
+//incremento (++)
+console.log(x++);
+console.log(x);
+
+//Decremento (--)
+console.log(--x);
