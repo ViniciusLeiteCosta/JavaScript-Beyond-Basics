47e58bfe9ce0fd4eddd76fc7667fc9bd451f8b8c
ViniciusLeiteCosta <viniciusleitecosta@gmail.com>
2023-11-22 13:59:39 -0300

    Adding lesson n7

---
+++ 7-arrays/javascript-7.js
@@ -0,0 +1,24 @@
+/*
+
+Ás vezes teremos que lidar com uma lista de objetos, bons exemplos para isto seriam:
+carrinhos de compra, lista de cores, lista de frutas, lista de nomes e etc.
+
+Para armazenar todos estes objetos nós vamos utilizar os Arrays!
+
+*/
+
+//Imagine uma lista de frutas!
+let banana = 'Banana';
+let maça = 'Maçã';
+let mamao = 'Mamão';
+
+let frutasEmVariaveis  = {banana, maça, mamao};
+
+console.log(frutasEmVariaveis);
+
+//Vamos tentar algo diferente!
+let frutinhas = ['Graviola', 'Abacaxi', 'Manga', 'Pitaya'];
+
+console.log(frutinhas);
+
+//Como pode ver, para variáveis usamos {}, já para valores []!
